31e39a24f3f3f883088f73ad7806662d0a28da7a
mateusaugust <mateusaugusto033@gmail.com>
2025-05-07 21:29:16 -0300

    feat: adição tabela nao tratada

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography, Link } from "@mui/material";
+import { DataNoFilterTable } from "../General Components/DataNoFilterTable";
 
 export const About = () => {
   return (
@@ -20,7 +21,7 @@ export const About = () => {
           gap: "10px",
           display: "flex",
           flexDirection: "column",
-          alignItems:'center'
+          alignItems: "center",
         }}
       >
         <Typography
@@ -29,7 +30,10 @@ export const About = () => {
           <b>Base de dados usada</b>
         </Typography>
 
-        <Link sx={{textAlign:"center"}} href="https://www.kaggle.com/datasets/timmofeyy/-coffee-rice-and-beef-price-changes-for-30-years">
+        <Link
+          sx={{ textAlign: "center" }}
+          href="https://www.kaggle.com/datasets/timmofeyy/-coffee-rice-and-beef-price-changes-for-30-years"
+        >
           ☕Coffee, Rice and Beef Prices Changes for 30 Years
         </Link>
 
@@ -40,21 +44,49 @@ export const About = () => {
           <br />
         </Typography>
 
-        <Typography sx={{color:'white', fontSize:'10px', textAlign:'justify'}}>
-        <br />A escolha desta base de dados se deu por sua consistência e
-          relevância em relação à minha atuação atual no mercado de trabalho.<br/>
+        <Typography
+          sx={{ color: "white", fontSize: "10px", textAlign: "justify" }}
+        >
+          <br />A escolha desta base de dados se deu por sua consistência e
+          relevância em relação à minha atuação atual no mercado de trabalho.
+          <br />
           Apesar de conter informações sobre diversas commodities, o café foi o
           principal foco de interesse devido à riqueza e profundidade dos dados
-          disponíveis. <br/>A base inclui variáveis fundamentais como preço, ano,
-          mês, inflação e o preço ajustado pela inflação, o que possibilita
-          análises mais precisas e contextualizadas.<br/> Esses dados serviram como
-          base para prever as tendências do mercado de café, permitindo que
-          usuários explorem a evolução dos preços ao longo do tempo.
+          disponíveis. <br />A base inclui variáveis fundamentais como preço,
+          ano, mês, inflação e o preço ajustado pela inflação, o que possibilita
+          análises mais precisas e contextualizadas.
+          <br /> Esses dados serviram como base para prever as tendências do
+          mercado de café, permitindo que usuários explorem a evolução dos
+          preços ao longo do tempo.
         </Typography>
 
-        <Link sx={{textAlign:"center"}} href="https://docs.google.com/document/d/1--YVrjdmaflTVIKWeFdowR8bbyJhfHki1RfTzL0l2lw/edit?usp=sharing">
-          Questões Respondidas 
+        <Link
+          sx={{ textAlign: "center" }}
+          href="https://docs.google.com/document/d/1--YVrjdmaflTVIKWeFdowR8bbyJhfHki1RfTzL0l2lw/edit?usp=sharing"
+        >
+          Questões Respondidas
         </Link>
+      </Box>
+
+      <Box
+        sx={{
+          backgroundColor: "#213435",
+          borderRadius: "10px",
+          boxShadow: 3,
+          padding: "15px",
+          gap: "10px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          sx={{ color: "white", textAlign: "center", fontSize: "15px" }}
+        >
+          <b>Base de Dados "Sem Tratamento"</b>
+        </Typography>
+
+        <DataNoFilterTable/>
       </Box>
     </Box>
   );
---
+++ src/General Components/DataNoFilterTable.tsx
@@ -0,0 +1,77 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Box,
+} from "@mui/material";
+
+export const DataNoFilterTable = () => {
+  const headerTable1 = Array<string>("Year", "Month", "Price_bife_kilo");
+  const headerTable2 = Array<string>("Price_rice_kilo", "Price_coffee_kilo", "Inflation_rate");
+  const headerTable3 = Array<string>("Price_rice_infl", "Price_beef_infl", "Price_coffee_infl");
+
+  const bodyTable1 = Array<string>("Number", "String", "Number");
+  const bodyTable2 = Array<string>("Number", "Number", "Number");
+  const bodyTable3 = Array<string>("Number", "Number", "Number");
+
+  
+  return (
+    <Box sx={{width:'100%', display:'flex', gap:'10px', flexDirection:'column', alignItems:'center'}}>
+      <Table sx={{ backgroundColor: "white" }}>
+        <TableHead>
+          <TableRow>
+            {headerTable1.map((head) => (
+              <TableCell sx={{ textAlign: "center" }}>{head}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          <TableRow>
+            {bodyTable1.map((body) => (
+              <TableCell sx={{ textAlign: "center" }}>{body}</TableCell>
+            ))}
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <Table sx={{ backgroundColor: "white" }}>
+        <TableHead>
+          <TableRow>
+            {headerTable2.map((head) => (
+              <TableCell sx={{ textAlign: "center" }}>{head}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          <TableRow>
+            {bodyTable2.map((body) => (
+              <TableCell sx={{ textAlign: "center" }}>{body}</TableCell>
+            ))}
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <Table sx={{ backgroundColor: "white" }}>
+        <TableHead>
+          <TableRow>
+            {headerTable3.map((head) => (
+              <TableCell sx={{ textAlign: "center" }}>{head}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          <TableRow>
+            {bodyTable3.map((body) => (
+              <TableCell sx={{ textAlign: "center" }}>{body}</TableCell>
+            ))}
+          </TableRow>
+        </TableBody>
+      </Table>
+    </Box>
+  );
+};
